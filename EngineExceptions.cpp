647d6ca5ac321d67ed0757c3b8aaab235750f3ce
k-o-white <koburilichev@gmail.com>
2023-04-03 19:18:59 +0400

    "EngineExceptions" header file changed and source file added

---
+++ EngineExceptions.cpp
@@ -0,0 +1,25 @@
+//
+// Created by teena on 03.04.2023.
+//
+
+#include "EngineExceptions.h"
+
+const char* ConfigFileMissingException::what() const noexcept
+{
+    return "Config file is missing.";
+}
+
+const char *ConfigFileEmptyException::what() const noexcept
+{
+    return "Config file is empty.";
+}
+
+const char *IncorrectVersionException::what() const noexcept
+{
+    return "\"config.json\" has incorrect file version.";
+}
+
+const char *RequestsFileMissingException::what() const noexcept
+{
+    return "Config file is missing.";
+}
--- EngineExceptions.h
+++ EngineExceptions.h
@@ -11,10 +11,7 @@ class ConfigFileMissingException : public std::exception
 public:
     ConfigFileMissingException() = default;
 
-    [[nodiscard]] const char* what() const noexcept override
-    {
-        return "Config file is missing.";
-    }
+    [[nodiscard]] const char* what() const noexcept override;
 };
 
 class ConfigFileEmptyException : public std::exception
@@ -22,10 +19,7 @@ class ConfigFileEmptyException : public std::exception
 public:
     ConfigFileEmptyException() = default;
 
-    [[nodiscard]] const char* what() const noexcept override
-    {
-        return "Config file is empty.";
-    }
+    [[nodiscard]] const char* what() const noexcept override;
 };
 
 class IncorrectVersionException : public std::exception
@@ -33,10 +27,7 @@ class IncorrectVersionException : public std::exception
 public:
     IncorrectVersionException() = default;
 
-    [[nodiscard]] const char* what() const noexcept override
-    {
-        return "\"config.json\" has incorrect file version.";
-    }
+    [[nodiscard]] const char* what() const noexcept override;
 };
 
 class RequestsFileMissingException : public std::exception
@@ -44,10 +35,7 @@ class RequestsFileMissingException : public std::exception
 public:
     RequestsFileMissingException() = default;
 
-    [[nodiscard]] const char* what() const noexcept override
-    {
-        return "Config file is missing.";
-    }
+    [[nodiscard]] const char* what() const noexcept override;
 };
 
 #endif //SEARCH_ENGINE_ENGINEEXCEPTIONS_H
