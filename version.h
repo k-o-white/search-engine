20d94555aa2d86e1054ae5cfa6a8bd81aebd15c4
k-o-white <koburilichev@gmail.com>
2023-04-03 11:34:26 +0400

    Create version.h

---
+++ version.h
@@ -0,0 +1,11 @@
+//
+// Created by teena on 30.03.2023.
+//
+
+#ifndef SEARCH_ENGINE_VERSION_H
+#define SEARCH_ENGINE_VERSION_H
+#include <string>
+
+const std::string VERSION = "0.1";
+
+#endif //SEARCH_ENGINE_VERSION_H
