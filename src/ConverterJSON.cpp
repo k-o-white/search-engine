fe7b486ed579c7543fba95eaf01acab68a39c99e
k-o-white <koburilichev@gmail.com>
2023-06-19 23:13:20 +0400

    Update ConverterJSON.cpp

--- src/ConverterJSON.cpp
+++ src/ConverterJSON.cpp
@@ -48,7 +48,7 @@ std::vector<std::string> ConverterJSON::getRequests()
 void ConverterJSON::putAnswers(const std::vector<std::vector<std::pair<int, float>>> &answers)
 {
     nlohmann::json jsonAnswers;
-    std::ofstream answersFile("../../config/answers.json");
+    std::ofstream answersFile(".\\config\\answers.json");
     int requestInd = 1;
     for (const auto& pairVec : answers)
     {
@@ -93,7 +93,7 @@ void ConverterJSON::readConfigJSON()
     nlohmann::json jsonConfig;
     try
     {
-        std::ifstream configFile("../../config/config.json");
+        std::ifstream configFile(".\\config\\config.json");
         if (!configFile.is_open())
             throw ConfigFileMissingException();
         configFile >> jsonConfig;
@@ -128,7 +128,7 @@ void ConverterJSON::readRequestsJSON()
     nlohmann::json jsonRequests;
     try
     {
-        std::ifstream requestsFile("../../config/requests.json");
+        std::ifstream requestsFile(".\\config\\requests.json");
         if (!requestsFile.is_open())
             throw RequestsFileMissingException();
         requestsFile >> jsonRequests;
